7d03b3e5e0873af1cf4641aab6d2e859ee4ffc72
jaonoctus <joaodias@noctus.org>
2022-12-21 11:58:46 -0300

    refactor: assert btrfs is installed

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,18 @@
-use std::process::{Command, Stdio};
-use execute::Execute;
-
-const BTRFS_PATH: &str = "/usr/bin/btrfs";
+use std::process::{Command, exit};
 
 fn assert_btrfs_is_installed() {
-    let mut btrfs = Command::new(BTRFS_PATH);
-    btrfs.arg("--version");
+    let output = Command::new("btrfs")
+        .arg("--version")
+        .output();
 
-    if btrfs.execute_check_exit_status_code(0).is_err() {
-        eprintln!("The path `{}` is not a correct BTRFS executable binary file.", BTRFS_PATH);
+    if output.is_err() == true {
+        println!("ERROR: Command 'btrfs' not found, but can be installed with:");
+        // TODO: check OS first
+        println!("$ sudo apt install btrfs-progs");
+        exit(1);
     }
 }
 
 fn main() {
     assert_btrfs_is_installed();
-
-    let mut btrfs = Command::new(BTRFS_PATH);
-    btrfs.arg("--version");
-    btrfs.stdout(Stdio::piped());
-    btrfs.stderr(Stdio::piped());
-
-    let output = btrfs.execute_output().unwrap();
-
-    if let Some(exit_code) = output.status.code() {
-        if exit_code == 0 {
-            println!("Ok.");
-        } else {
-            eprintln!("Failed.");
-        }
-    } else {
-        eprintln!("Interrupted!");
-    }
-    
-    println!("{}", String::from_utf8(output.stdout).unwrap());
-    println!("{}", String::from_utf8(output.stderr).unwrap());
 }
