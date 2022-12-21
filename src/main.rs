6e7b111c6fce4ebdf0036d44ea8d7f2c146f160b
jaonoctus <joaodias@noctus.org>
2022-12-21 02:25:53 -0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
-use std::env;
+use std::process::{Command, Stdio};
+use execute::Execute;
 
-fn get_current_path() -> String {
-    let path = env::current_dir().unwrap();
+const BTRFS_PATH: &str = "/usr/bin/btrfs";
 
-    path.display().to_string()
+fn assert_btrfs_is_installed() {
+    let mut btrfs = Command::new(BTRFS_PATH);
+    btrfs.arg("--version");
+
+    if btrfs.execute_check_exit_status_code(0).is_err() {
+        eprintln!("The path `{}` is not a correct BTRFS executable binary file.", BTRFS_PATH);
+    }
 }
 
 fn main() {
-    // TODO:
-    // ask for path (default=current)
-    // show options
+    assert_btrfs_is_installed();
 
-    // subvolume: create, snapshot, remove
-    let _path = "";
+    let mut btrfs = Command::new(BTRFS_PATH);
+    btrfs.arg("--version");
+    btrfs.stdout(Stdio::piped());
+    btrfs.stderr(Stdio::piped());
 
-    println!("{:?}", get_current_path())
+    let output = btrfs.execute_output().unwrap();
+
+    if let Some(exit_code) = output.status.code() {
+        if exit_code == 0 {
+            println!("Ok.");
+        } else {
+            eprintln!("Failed.");
+        }
+    } else {
+        eprintln!("Interrupted!");
+    }
+    
+    println!("{}", String::from_utf8(output.stdout).unwrap());
+    println!("{}", String::from_utf8(output.stderr).unwrap());
 }
